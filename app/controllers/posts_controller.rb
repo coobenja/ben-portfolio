456336a4099786d1a6a54f972fc0b95433be21e3
coobenja <coobenja@umich.edu>
2015-09-08 13:06:47 -0400

    Added download field, accomp tweaks WIP

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,7 +48,7 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :text, :avatar, :video_url)
+      params.require(:post).permit(:title, :text, :avatar, :video_url, :download_link)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907020500) do
+ActiveRecord::Schema.define(version: 20150908000947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150907020500) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "video_url"
+    t.string   "download_link"
   end
 
   create_table "users", force: :cascade do |t|
