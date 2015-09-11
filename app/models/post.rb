865e9fb5212b782cc0f79e3da6563a537d9d14fb
coobenja <coobenja@umich.edu>
2015-09-10 21:01:47 -0400

    Game ordering

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 5 }
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
